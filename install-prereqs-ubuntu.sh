7de9a2ec1cdaeea89ee88fcadedea702183e5dd2
xinyu.chen <you@example.com>
2025-04-20 03:22:06 +0000

    chore: add installation of cargo

--- install-prereqs-ubuntu.sh
+++ install-prereqs-ubuntu.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo apt-get install g++ linux-libc-dev libclang-dev unzip libjemalloc-dev make -y
+curl https://sh.rustup.rs -sSf | sudo sh # install cargo
+source ~/.cargo/env
 cargo install cargo-nextest
